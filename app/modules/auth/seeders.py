00f2784edb6d73c95ecce453eef6bc04e17bb47e
Ariel Escobar Capilla <ariesccap@alum.us.es>
2025-11-10 00:08:31 +0100

    test: Create all tests (integration cov 98%)

--- app/modules/auth/seeders.py
+++ app/modules/auth/seeders.py
@@ -1,7 +1,8 @@
 from app.modules.auth.models import User
+from app.modules.auth.services import AuthenticationService, encrypt_data
 from app.modules.profile.models import UserProfile
 from core.seeders.BaseSeeder import BaseSeeder
-
+from flask import current_app
 
 class AuthSeeder(BaseSeeder):
 
@@ -35,3 +36,34 @@ class AuthSeeder(BaseSeeder):
 
         # Seeding user profiles
         self.seed(user_profiles)
+
+
+        # Usuario para tests de 2FA
+        KNOWN_2FA_SECRET = "5JEIF3ANYS7UJKZEN7PZJFG5RHTNRPR2" # Clave conocida para tests
+
+        encrypted_secret = None
+        hashed_codes = None
+
+        with current_app.app_context():
+            encrypted_secret = encrypt_data(KNOWN_2FA_SECRET)
+            
+            auth_service = AuthenticationService()
+            _, hashed_codes = auth_service.generate_recovery_codes()
+
+        user_2fa = User(
+            email="test@example.com",
+            password="test1234", 
+            is_two_factor_enabled=True,
+            two_factor_secret=encrypted_secret, 
+            two_factor_recovery_codes=hashed_codes
+        )
+        
+        seeded_2fa_user = self.seed([user_2fa])
+
+        profile_data_2fa = {
+            "user_id": seeded_2fa_user[0].id,
+            "name": "Test",
+            "surname": "User",
+        }
+        user_profile_2fa = UserProfile(**profile_data_2fa)
+        self.seed([user_profile_2fa])
