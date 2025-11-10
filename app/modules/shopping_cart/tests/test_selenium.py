d628a7fc672688b6cbe1e4080a52d687c3031889
Esteban Lopez Perez <estlopper@alum.us.es>
2025-11-10 16:05:32 +0100

    feat: Test unitarios y de interfaz del módulo shopping_cart.
    Además se completa sin problemas los test de carga en Locust.

--- app/modules/shopping_cart/tests/test_selenium.py
+++ app/modules/shopping_cart/tests/test_selenium.py
@@ -1,36 +1,39 @@
-import time
-
-from selenium.common.exceptions import NoSuchElementException
-
-from core.environment.host import get_host_for_selenium_testing
-from core.selenium.common import close_driver, initialize_driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
-def test_shopping_cart_index():
+class Test3:
 
-    driver = initialize_driver()
+    def setup_method(self, method):
+        self.driver = webdriver.Firefox()
+        self.vars = {}
 
-    try:
-        host = get_host_for_selenium_testing()
+    def teardown_method(self, method):
+        self.driver.quit()
 
-        # Open the index page
-        driver.get(f"{host}/shopping_cart")
-
-        # Wait a little while to make sure the page has loaded completely
-        time.sleep(4)
-
-        try:
-
-            pass
-
-        except NoSuchElementException:
-            raise AssertionError("Test failed!")
-
-    finally:
-
-        # Close the browser
-        close_driver(driver)
-
-
-# Call the test function
-test_shopping_cart_index()
+    def test_3(self):
+        self.driver.get("http://127.0.0.1:5000/")
+        self.driver.set_window_size(1014, 754)
+        self.driver.find_element(By.LINK_TEXT, "Login").click()
+        self.driver.find_element(By.ID, "submit").click()
+        self.driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
+        self.driver.find_element(By.LINK_TEXT, "Login").click()
+        self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
+        self.driver.find_element(By.ID, "password").send_keys("1234")
+        self.driver.find_element(By.ID, "submit").click()
+        self.driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
+        self.driver.find_element(By.LINK_TEXT, "Add to cart").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-close:nth-child(1)").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".list-group-item:nth-child(4) .btn-outline-success").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".nav-icon > .feather-shopping-cart").click()
+        self.driver.find_element(By.LINK_TEXT, "Remove").click()
+        self.driver.find_element(By.LINK_TEXT, "Vaciar Carrito").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-close:nth-child(1)").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".nav-item:nth-child(1) > .nav-icon").click()
+        self.driver.find_element(By.LINK_TEXT, "My datasets").click()
+        self.driver.find_element(By.LINK_TEXT, "Sample dataset 1").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".list-group-item:nth-child(4) .btn-outline-success").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".list-group-item:nth-child(4) .btn-outline-success").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".nav-icon > .feather-shopping-cart").click()
+        self.driver.find_element(By.LINK_TEXT, "Vaciar Carrito").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".text-dark").click()
