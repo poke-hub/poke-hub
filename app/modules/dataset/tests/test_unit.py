4c7fc7f93fc5a2e6fffa00b6e6e2d77199d8e8eb
José Ángel Rodríguez Durán <jarguezduran@gmail.com>
2025-11-10 19:29:46 +0100

    fix: corregida construcción errónea de los datasets de las funciones de los tests

--- app/modules/dataset/tests/test_unit.py
+++ app/modules/dataset/tests/test_unit.py
@@ -1,6 +1,7 @@
 import pytest
-from app.modules.feature_model.models import FeatureModel, FMMetaData
-from app.modules.dataset.models import DSMetaData, Dataset
+from unittest.mock import MagicMock
+from app.modules.featuremodel.models import FeatureModel, FMMetaData
+from app.modules.dataset.models import DSMetaData, DataSet, Author
 
 
 @pytest.fixture(scope="module")
@@ -28,47 +29,55 @@ def test_sample_assertion(test_client):
 def test_ds_meta_data_get_all_tags():
 
     metadata = DSMetaData(tags="tag1,tag2,tag3")
+    dataset = DataSet(ds_meta_data=metadata)
     tags = metadata.get_all_tags()
-    assert tags == ["tag1", "tag2", "tag3"], "The tags extracted do not match the expected list."
+    assert tags == {"tag1", "tag2", "tag3"}, "The tags extracted do not match the expected set."
 
     fm_meta_data = FMMetaData(tags="tag3,tag5")
     fm_meta_data2 = FMMetaData(tags="tag4")
     fm1 = FeatureModel(fm_meta_data=fm_meta_data)
     fm2 = FeatureModel(fm_meta_data=fm_meta_data2)
-    metadata_complex = DSMetaData(tags=" tag1 , tag2")
-    Dataset = Dataset(feature_models=[fm1, fm2], ds_meta_data=metadata)
+    metadata_complex = DSMetaData(tags="tag1 , tag2")
+    dataset = DataSet(feature_models=[fm1, fm2], ds_meta_data=metadata_complex)
     tags_complex = metadata_complex.get_all_tags()
-    assert tags_complex == ["tag1", "tag2", "tag3", "tag4", "tag5"], "The tags extracted with spaces do not match the expected list."
+    assert tags_complex == {"tag1", "tag2", "tag3", "tag4", "tag5"}, "The tags extracted with spaces do not match the expected set."
 
     metadata_empty = DSMetaData(tags="")
+    dataset = DataSet(ds_meta_data=metadata_empty)
     tags_empty = metadata_empty.get_all_tags()
-    assert tags_empty == [], "The tags extracted from an empty string should be an empty list."
+    assert tags_empty == set(), "The tags extracted from an empty string should be an empty set."
 
 def test_ds_meta_data_get_all_authors():
 
-    metadata = DSMetaData(authors="author1,author2")
+    author1 = Author(name="author1")
+    author2 = Author(name="author2")
+    metadata = DSMetaData(authors=[author1, author2])
+    dataset = DataSet(ds_meta_data=metadata)
     authors = metadata.get_all_authors()
-    assert authors == ["author1", "author2"], "The authors extracted do not match the expected list."
+    assert authors == {author1, author2}, "The authors extracted do not match the expected set."
 
-    fm_meta_data = FMMetaData(authors="author1,author2")
-    fm_meta_data2 = FMMetaData(authors="author3")
+    author3 = Author(name="author3")
+    author4 = Author(name="author4")
+    fm_meta_data = FMMetaData(authors=[author1, author2])
+    fm_meta_data2 = FMMetaData(authors=[author3])
     fm1 = FeatureModel(fm_meta_data=fm_meta_data)
     fm2 = FeatureModel(fm_meta_data=fm_meta_data2)
-    metadata_complex = DSMetaData(authors=" author1 , author4 ")
-    Dataset = Dataset(feature_models=[fm1, fm2], ds_meta_data=metadata)
+    metadata_complex = DSMetaData(authors=[author1, author4])
+    dataset = DataSet(feature_models=[fm1, fm2], ds_meta_data=metadata_complex)
     authors_complex = metadata_complex.get_all_authors()
-    assert authors_complex == ["author1", "author2", "author3", "author4"], "The authors extracted with spaces do not match the expected list."
+    assert authors_complex == {author1, author2, author3, author4}, "The authors extracted with spaces do not match the expected set."
 
-    metadata_empty = DSMetaData(authors="")
+    metadata_empty = DSMetaData(authors=[])
+    dataset = DataSet(ds_meta_data=metadata_empty)
     authors_empty = metadata_empty.get_all_authors()
-    assert authors_empty == [], "The authors extracted from an empty string should be an empty list."
+    assert authors_empty == set(), "The authors extracted from an empty list should be an empty set."
 
 def test_ds_meta_data_has_tag():
 
     metadata = DSMetaData(tags="tag1,tag3")
     fm_meta_data = FMMetaData(tags="tag2,tag3")
     fm = FeatureModel(fm_meta_data=fm_meta_data)
-    Dataset = Dataset(feature_models=[fm], ds_meta_data=metadata)
+    dataset = DataSet(feature_models=[fm], ds_meta_data=metadata)
 
     assert metadata.has_tag("tag1") is True, "The tag 'tag1' should be found."
     assert metadata.has_tag("tag2") is True, "The tag 'tag2' should be found."
@@ -76,11 +85,15 @@ def test_ds_meta_data_has_tag():
 
 def test_ds_meta_data_has_author():
 
-    metadata = DSMetaData(authors="author1,author3")
-    fm_meta_data = FMMetaData(authors="author2,author3")
+    author1 = Author(id=1, name="author1")
+    author2 = Author(id=2, name="author2")
+    author3 = Author(id=3, name="author3")
+    author4 = Author(id=4, name="author4")
+    metadata = DSMetaData(authors=[author1, author3])
+    fm_meta_data = FMMetaData(authors=[author2, author3])
     fm = FeatureModel(fm_meta_data=fm_meta_data)
-    Dataset = Dataset(feature_models=[fm], ds_meta_data=metadata)
+    dataset = DataSet(feature_models=[fm], ds_meta_data=metadata)
 
-    assert metadata.has_author("author1") is True, "The author 'author1' should be found."
-    assert metadata.has_author("author2") is True, "The author 'author2' should be found."
-    assert metadata.has_author("author4") is False, "The author 'author4' should not be found."
+    assert metadata.has_author(1) is True, "The author 'author1' should be found."
+    assert metadata.has_author(2) is True, "The author 'author2' should be found."
+    assert metadata.has_author(4) is False, "The author 'author4' should not be found."
