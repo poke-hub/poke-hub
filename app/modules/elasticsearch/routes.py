43858c0ea3f364d314ce4862decd5e081f0ca8d0
José Ángel Rodríguez Durán <jarguezduran@gmail.com>
2025-11-15 12:25:56 +0100

    fix: corregido código por lint

--- app/modules/elasticsearch/routes.py
+++ app/modules/elasticsearch/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template
+
 from app.modules.elasticsearch import elasticsearch_bp
 
 
-@elasticsearch_bp.route('/elasticsearch', methods=['GET'])
+@elasticsearch_bp.route("/elasticsearch", methods=["GET"])
 def index():
-    return render_template('elasticsearch/index.html')
+    return render_template("elasticsearch/index.html")
