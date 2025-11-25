8da7358c4c9ec89fe133228953ab8300484a20ed
Jesus-Pons <jesponmor@alum.us.es>
2025-11-25 23:25:30 +0100

    test: Añadido test de selenium descarga carrito
    Se ha añadido un test de selenium para comprobar el correcto funcionamiento de la descarga de tu carrito

--- app/modules/shopping_cart/tests/test_selenium.py
+++ app/modules/shopping_cart/tests/test_selenium.py
@@ -37,3 +37,33 @@ class Test3:
         self.driver.find_element(By.CSS_SELECTOR, ".nav-icon > .feather-shopping-cart").click()
         self.driver.find_element(By.LINK_TEXT, "Vaciar Carrito").click()
         self.driver.find_element(By.CSS_SELECTOR, ".text-dark").click()
+
+
+class TestTestseleniumdownloadcart:
+    def setup_method(self, method):
+        self.driver = webdriver.Firefox()
+        self.vars = {}
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_testseleniumdownloadcart(self):
+        self.driver.get("http://localhost:5000/")
+        self.driver.set_window_size(1920, 1048)
+        self.driver.find_element(By.CSS_SELECTOR, ".nav-link:nth-child(1)").click()
+        self.driver.find_element(By.ID, "email").click()
+        self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
+        self.driver.find_element(By.ID, "password").click()
+        self.driver.find_element(By.ID, "password").send_keys("1234")
+        self.driver.find_element(By.ID, "submit").click()
+        self.driver.find_element(By.LINK_TEXT, "Explore").click()
+        self.driver.find_element(By.LINK_TEXT, "Sample dataset 1").click()
+        self.driver.find_element(By.LINK_TEXT, "Add to cart").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, ".list-group > .list-group-item > .list-group-item > .row .btn-outline-success"
+        ).click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, ".list-group-item > .list-group-item > .list-group-item .btn-outline-success"
+        ).click()
+        self.driver.find_element(By.CSS_SELECTOR, ".nav-icon > .feather-shopping-cart > path").click()
+        self.driver.find_element(By.LINK_TEXT, "Descargar Todo").click()
