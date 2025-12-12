2d3049d2bfaf78cd56873dbaebb28f2d3601045e
Ariel Escobar Capilla <ariesccap@alum.us.es>
2025-12-12 13:40:45 +0100

    style: Lint

--- app/modules/profile/routes.py
+++ app/modules/profile/routes.py
@@ -14,9 +14,7 @@ from app.modules.profile.services import UserProfileService
 @login_required
 def edit_profile():
     auth_service = AuthenticationService()
-    profile = (
-        auth_service.get_authenticated_user_profile()
-    )
+    profile = auth_service.get_authenticated_user_profile()
     if not profile:
         return redirect(url_for("public.index"))
 
